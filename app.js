421d9c7e58db83e5d8ad167c22634ffa1d14cee3
Jitarth Rai <jitarthr@gmail.com>
2023-07-27 22:40:56 +0530

    add process port to app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 var tasks =[];
 var tasksWork =[];
 const d = new Date();
